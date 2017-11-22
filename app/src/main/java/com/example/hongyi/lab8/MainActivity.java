80cf64f3dad8f0a520b29bfed93cc49403204bfe
pandelaz <pandelaz@gmail.com>
2017-11-22 13:06:54 +0800

    first commit

---
+++ app/src/main/java/com/example/hongyi/lab8/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.hongyi.lab8;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        if(button!=null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this,MyService.class);
+                    startService(intent);
+                    Toast.makeText(MainActivity.this,"啟動Service",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            });
+        }
+
+
+
+    }
+}
